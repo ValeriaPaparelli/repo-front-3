b05b8c9b848c468fb4998ca6764cdac0d60ce27f
Valeria Paparelli <valeria.paparelli@live.com.ar>
2023-03-05 16:38:35 -0300

    repaso map y useState

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 // import App from "./products/App";
 //import App from "./contador/App";
-import App from "./digitalProducts/App";
+//import App from "./digitalProducts/App";"
+import App from "./sportProducts/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
