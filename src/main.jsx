64c749f792e39aa937f2f99ab80dfc82a81f2c34
Valeria Paparelli <valeria.paparelli@live.com.ar>
2023-03-06 15:26:58 -0300

    eventos en formularios

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom/client";
 // import App from "./products/App";
 //import App from "./contador/App";
 //import App from "./digitalProducts/App";"
-import App from "./sportProducts/App";
+//import App from "./sportProducts/App";
+import App from "./Form/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
