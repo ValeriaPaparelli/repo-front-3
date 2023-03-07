1807098786388481ed3d1cfedade82d5cccdc976
Valeria Paparelli <valeria.paparelli@live.com.ar>
2023-03-07 17:42:15 -0300

    formulario con select

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import ReactDOM from "react-dom/client";
 //import App from "./contador/App";
 //import App from "./digitalProducts/App";"
 //import App from "./sportProducts/App";
-import App from "./Form/App";
+//import App from "./Form/App";
+import App from "./Select/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
